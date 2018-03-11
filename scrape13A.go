1210b6cee2291675842fa4124a18633089eea781
toferc <cgeist7@gmail.com>
2018-03-11 00:05:45 -0500

    updated scraper to use Colly and started framing pull.

--- scrape13A.go
+++ scrape13A.go
@@ -1,49 +1,89 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"log"
 
-  "github.com/yhat/scrape"
-  "golang.org/x/net/html"
-  "golang.org/x/net/html/atom"
+	"github.com/gocolly/colly"
 )
 
 var class_list = map[string]int{
-  "barbarian": 12,
-  "fighter": 10,
-  "rogue": 6,
-  "wizard": 4,
+	"barbarian": 12,
+	"fighter":   10,
+	"rogue":     6,
+	"wizard":    4,
+}
+
+type Class struct {
+	Name         string
+	Melee        string
+	Range        string
+	Armour       []string
+	HP           string
+	AbilityPicks map[int]map[string]int
+	Abilities    map[string]string
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
-  // request and parse front page
 
-  class := "barbarian"
+	class := "barbarian"
+	path := "http://www.13thagesrd.com/classes/" + class
+	omit := []string{
+		"Gold Pieces", "Navigation", " Latest products in the Open Gaming Store"}
 
-  path := "http://www.13thagesrd.com/classes/" + class
+	abilities := make(map[string]string)
 
-  resp, err := http.Get(path)
-  if err != nil {
-    panic(err)
-  }
-  root, err := html.Parse(resp.Body)
-  if err != nil {
-    panic(err)
-  }
+	// Initialize Colly Collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.13thagesrd.com"),
+	)
 
-  // set a map
-  var abilityScrape = make(map[string]string)
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 
-  // grab all abilities and printCharacter
-  abilities := scrape.FindAllNested(root, scrape.ByTag(atom.H4))
-  for _, ability := range abilities {
-    fmt.Println(scrape.Text(ability))
-    abilityScrape[scrape.Text(ability)] = "default"
-    for _, details := range scrape.FindAllNested(ability, scrape.ByTag(atom.P)) {
-      fmt.Println(scrape.Text(ability))
-      abilityScrape[scrape.Text(ability)] = scrape.Text(details)
-    }
-  }
-  fmt.Println(abilityScrape)
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+	})
+
+	/* Scraping and traversing
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	}) */
+
+	c.OnHTML("h4", func(e *colly.HTMLElement) {
+		if stringInSlice(e.Text, omit) {
+		} else {
+			fmt.Println("Found Ability:", e.Text)
+			description := e.ChildText("p")
+			abilities[e.Text] = description
+		}
+	})
+
+	/* Test scraping function - table
+	c.OnHTML("tr td", func(e *colly.HTMLElement) {
+		fmt.Println("table row", e.Text)
+	}) */
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
+	c.Visit(path)
+
+	fmt.Println(abilities)
+
 }
